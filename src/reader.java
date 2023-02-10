0225e3dbc43b64904a62fd0e49d4ae0c5ccfbf74
qkral <qkralsrl214@naver.com>
2023-02-11 00:22:39 +0900

    2

--- src/reader.java
+++ src/reader.java
@@ -4,32 +4,45 @@ public class reader {
     public int solution(int num , int[][]arr)
     {
         int answer = 0;
-        int[]score = new int [num];
-        int target = 0;
+        int big = Integer.MIN_VALUE;
         for(int i = 0 ;i<num ; i++)
         {
-
+            int count = 0;
             for(int j = 0 ; j<num;j++)
             {
-
+                for(int k =0;k<5;k++)
+                {
+                    if(arr[i][k] == arr[j][k])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            if(count>big)
+            {
+                big = count;
+                answer = i+1;
             }
         }
+
         return answer;
     }
 
     public static void main (String []argv)
     {
+        reader rd = new reader();
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        int [][] arr = new int[num][num];
-        for(int i = 0 ;i<num ; i++)
+        int [][] arr = new int[num][5];
+        for(int i = 0 ;i<5 ; i++)
         {
             for(int j = 0 ; j<num;j++)
             {
                 arr[i][j] = sc.nextInt();
             }
         }
-
+        System.out.print(rd.solution(num , arr));
 
     }
 
