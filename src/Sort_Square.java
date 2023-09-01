70fd6b60d8ba424f0a4a1c81b9790da4a8e3b8a7
박민기 <86062121+mingki1242@users.noreply.github.com>
2023-09-01 21:37:44 +0900

    Square_Fin

--- src/Sort_Square.java
+++ src/Sort_Square.java
@@ -1,2 +1,40 @@
-public class Sort_Square z{
+import java.lang.reflect.Array;
+import java.util.*;
+class Point implements Comparable<Point>
+{
+    public int x;
+    public int y;
+    Point(int x , int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    @Override // compareTo함수 재정의
+    public int compareTo(Point o)
+    {
+        if(this.x == o.x) return this.y - o.y; //x값이 같을 경우 y좌표를 비교하여 오름차순 정렬 일 경우 양수 리턴
+        else return this.x - o.x; //아닐경우 x값을 비교하여 오름차순 정렬 일 경우 양수 리턴
+    }
+}
+class Sort_Square{
+
+    public static void main(String [] argv) {
+        Sort_Square square = new Sort_Square();
+        Scanner sc = new Scanner(System.in);
+        int Size = sc.nextInt();
+        ArrayList<Point> arr = new ArrayList<>();
+        for(int i = 0 ; i<Size ; i++)
+        {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            arr.add(new Point(x,y));
+        }
+        //Collections.sort() 또는 Arrays.sort()와 같은 정렬 메서드는 compareTo 메서드를 호출하여 객체 간의 비교를 수행하며, 이를 기반으로 정렬을 수행합니다
+        Collections.sort(arr);
+        for(Point o : arr)
+        {
+            System.out.println(o.x + " " + o.y);
+        }
+    }
+
 }
